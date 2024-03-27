9e20149a14eca4843d6c7d3825a14bd902c0e6a9
Maxime CARON <145995231+MaximeAlgosup@users.noreply.github.com>
2024-03-27 14:44:18 +0100

    Init framework

---
+++ coding-dojo/controllers/default.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "beego.vip"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+
+func (main *MainController) HelloSitepoint() {
+	main.Data["Website"] = "My Website"
+	main.Data["Email"] = "your.email.address@example.com"
+	main.Data["EmailName"] = "Your Name"
+	main.TplName = "hello-sitepoint.tpl"
+}
